adafe8505024e850209e4e18b742ecabdbf0b929
Atendo Log BR <contato@atendo.log.br>
2026-01-24 11:30:44 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   base: './',
   define: {
     // Injeção direta da Chave de API fornecida para garantir funcionamento imediato
-    'process.env.API_KEY': JSON.stringify("AIzaSyB-vO2vH6m6G9r7m7v8z2x1c5v4b3n2m1")
+    'process.env.API_KEY': JSON.stringify("COLQUE-AQUI-SUA-CHAVE-API")
   },
   build: {
     outDir: 'dist',
