c7e3ac072b032145c896561b0edee3df8c84b83e
Atendo Log BR <contato@atendo.log.br>
2026-01-24 11:33:12 -0300

    Update prospectService.ts

--- services/prospectService.ts
+++ services/prospectService.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 
 export async function getProspects(query: string, location: string) {
-  const res = await axios.get("http://localhost:4000/api/prospect", {
-    params: { query, location },
-  });
-  return res.data;
+  try {
+    const res = await axios.get("http://localhost:4000/api/prospect", {
+      params: { query, location },
+    });
+    return res.data;
+  } catch (error) {
+    console.error("Erro ao buscar prospects:", error);
+    return [];
+  }
 }
